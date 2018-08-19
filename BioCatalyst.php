96d03ce09c3a609e4d95ec70f151321357c7c5f9
Andrew Martin <andy123@stanford.edu>
2018-08-19 09:41:01 -0700

    renamed log to emLog

--- BioCatalyst.php
+++ BioCatalyst.php
@@ -294,20 +294,20 @@ class BioCatalyst extends \ExternalModules\AbstractExternalModule
 
     function emLog() {
         $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        $emLogger->log($this->PREFIX, func_get_args(), "INFO");
+        $emLogger->emLog($this->PREFIX, func_get_args(), "INFO");
     }
 
     function emDebug() {
         // Check if debug enabled
         if ($this->getSystemSetting('enable-system-debug-logging') || $this->getProjectSetting('enable-project-debug-logging')) {
             $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-            $emLogger->log($this->PREFIX, func_get_args(), "DEBUG");
+            $emLogger->emLog($this->PREFIX, func_get_args(), "DEBUG");
         }
     }
 
     function emError() {
         $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        $emLogger->log($this->PREFIX, func_get_args(), "ERROR");
+        $emLogger->emLog($this->PREFIX, func_get_args(), "ERROR");
     }
 
 }
